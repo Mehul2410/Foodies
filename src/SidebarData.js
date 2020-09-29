e50262d14d4317046e18c2fa6c558faa3d539db3
Mehul Gawhale <62436745+Mehul2410@users.noreply.github.com>
2020-09-29 10:08:27 -0700

    Update SidebarData.js

--- src/SidebarData.js
+++ src/SidebarData.js
@@ -5,27 +5,27 @@ import * as IoIcons from "react-icons/io";
 
 export const SidebarData = [
   {
-    title: "HOME",
+    title: "Home",
     path: "/",
     icon: <AiIcons.AiFillHome />,
     cName: "nav-text",
   },
   {
-    title: "RECIPES & MENUS",
-    path: "/Search",
+    title: "Nutrition Information",
+    path: "/nutrition",
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
   },
-  {
-    title: "INGREDIENTS",
-    path: "/ingredients",
-    icon: <FaIcons.FaCartPlus />,
-    cName: "nav-text",
-  },
-  {
-    title: "HOLIDAYS & EVENTS",
-    path: "/events",
-    icon: <IoIcons.IoMdPeople />,
-    cName: "nav-text",
-  },
+  // {
+  //   title: "INGREDIENTS",
+  //   path: "/ingredients",
+  //   icon: <FaIcons.FaCartPlus />,
+  //   cName: "nav-text",
+  // },
+  // {
+  //   title: "HOLIDAYS & EVENTS",
+  //   path: "/events",
+  //   icon: <IoIcons.IoMdPeople />,
+  //   cName: "nav-text",
+  // },
 ];
