1c99dbcf667d6026219c36bbc237c96f8eff5365
Mehul Gawhale <62436745+Mehul2410@users.noreply.github.com>
2020-09-29 10:04:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.css";
 import Search from "./Search";
 import Header from "./Header.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ExpertadviceM from "./ExpertadviceM";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
+          <Route path="/nutrition">
+            <Header />
+            <ExpertadviceM />
+          </Route>
           <Route path="/">
             <Header />
             <Search />
           </Route>
-          <Route path="/about"></Route>
         </Switch>
       </Router>
     </div>
